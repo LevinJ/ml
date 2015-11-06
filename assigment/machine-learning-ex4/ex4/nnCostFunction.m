5b492a9b916369e47136048f12714e333dac047a
Levin Jian <jianzhirong@gmail.com>
2015-11-06 13:43:23 +0800

    neural netowrk regularized gradient

--- assigment/machine-learning-ex4/ex4/nnCostFunction.m
+++ assigment/machine-learning-ex4/ex4/nnCostFunction.m
@@ -83,7 +83,9 @@ for m_index=1:m
     Theta2_grad = Theta2_grad + delta3 * a2_bias';    
 end
 Theta1_grad = Theta1_grad /m;
+Theta1_grad(:,2:end) = Theta1_grad(:,2:end) + (lambda * Theta1(:,2:end))/m;
 Theta2_grad = Theta2_grad /m;
+Theta2_grad(:,2:end) = Theta2_grad(:,2:end) + (lambda * Theta2(:,2:end))/m;
 
 J = J/m;
 
