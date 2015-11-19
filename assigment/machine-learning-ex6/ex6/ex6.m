f8908e552ab0f4a9eb86b04d50e2a0891dbaf4e3
Levin Jian <jianzhirong@gmail.com>
2015-11-19 14:19:59 +0800

    Gaussian kernel

--- assigment/machine-learning-ex6/ex6/ex6.m
+++ assigment/machine-learning-ex6/ex6/ex6.m
@@ -35,7 +35,7 @@ load('ex6data1.mat');
 plotData(X, y);
 
 fprintf('Program paused. Press enter to continue.\n');
-pause;
+%pause;
 
 %% ==================== Part 2: Training Linear SVM ====================
 %  The following code will train a linear SVM on the dataset and plot the
@@ -51,11 +51,12 @@ fprintf('\nTraining Linear SVM ...\n')
 % You should try to change the C value below and see how the decision
 % boundary varies (e.g., try C = 1000)
 C = 1;
+%C = 1000;
 model = svmTrain(X, y, C, @linearKernel, 1e-3, 20);
 visualizeBoundaryLinear(X, y, model);
 
 fprintf('Program paused. Press enter to continue.\n');
-pause;
+%pause;
 
 %% =============== Part 3: Implementing Gaussian Kernel ===============
 %  You will now implement the Gaussian kernel to use
