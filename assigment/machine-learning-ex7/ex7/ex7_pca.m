68bd4b94cd6c91554b45b90e635f44bbee84e0d1
Levin Jian <jianzhirong@gmail.com>
2015-11-24 16:31:34 +0800

    project data pca

--- assigment/machine-learning-ex7/ex7/ex7_pca.m
+++ assigment/machine-learning-ex7/ex7/ex7_pca.m
@@ -65,7 +65,7 @@ fprintf(' U(:,1) = %f %f \n', U(1,1), U(2,1));
 fprintf('\n(you should expect to see -0.707107 -0.707107)\n');
 
 fprintf('Program paused. Press enter to continue.\n');
-pause;
+%pause;
 
 
 %% =================== Part 3: Dimension Reduction ===================
